7b434313506f7a027519b500e633df8a12f0cee0
ofrifox <ofrifo@post.bgu.ac.il>
2022-01-02 20:10:11 +0200

    plot relevant experience

--- model_training_functions/kfold_cross_validation.py
+++ model_training_functions/kfold_cross_validation.py
@@ -14,6 +14,23 @@ def train_model_by_kfold(df, model):
         score = model.score(X_test, y_test)
         scores.append(score)
         print(score)
-
+    
     print('\n\nCross-Validation accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+    return  np.mean(scores)
 
+
+def train_model_by_kfold_depth(df, model):
+    kf = KFold(n_splits=10)
+    X = df.drop(columns=['target'])
+    y = df['target']
+    scores = []
+    for train_index, test_index in kf.split(df):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        model.fit(X_train,y_train)
+        score = model.score(X_test, y_test)
+        scores.append(score)
+        print(score)
+    
+    print('\n\nCross-Validation accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+    return  np.mean(scores)
